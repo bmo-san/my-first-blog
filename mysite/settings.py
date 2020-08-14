39f6c8f01f5b0520ef5417d3438649e877a14783
bmo-san <git.self.reference.engine@gmail.com>
2020-08-15 08:18:41 +0900

    postgres apply fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'djangogirls',
         'USER': 'bmo',
-        'PASSWORD': '',
+        'PASSWORD': 'bmo',
         'HOST': 'localhost',
         'PORT': '',
     }
